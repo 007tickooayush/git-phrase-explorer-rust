f1616df17e0a1e95cbb7dc27a0c3b7863fdbe03d
007tickooayush <ayush.tickoo292000@gmail.com>
2026-04-04 08:06:27 +0530

    formatting and refactoring

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("\n-----------------------------------------------------\n");
 
     // X------------------------------------------ EXTERNAL VARIABLES ------------------------------------------X
-
-    // // testing basic implementation:
-    // let repo_path = "/home/hellsent/PRJs/REACT/portfolio-vite-react";
-    // let file_path =  "src/_components/About.jsx"; // "src/App.jsx";
-    // let check_phrase = "<Text>{degree.degree}: {degree.major}</Text>";
-
     let repo_path = "/home/hellsent/ZedProjects/git-phrase-explorer/git-commits-track-test";
     let file_path =  "file1.txt"; // "src/App.jsx";
     let check_phrase = "UPDATED FILE IN branch2 changes";
     let mut result_phrase_line = String::new();
     let single_discovery = true;
-
     // X-----------------------------------------X EXTERNAL VARIABLES X-----------------------------------------X
 
     let repo = Repository::open(repo_path)?;
@@ -34,7 +27,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let oid = oid?;
         let commit = repo.find_commit(oid)?;
 
-        // if commit.parent_count() != 1 { continue; } // Skips merges/initial commits
         let mut has_parent = false;
         if commit.parent_count() > 0 {
             has_parent = true;
@@ -86,7 +78,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         );
 
                         // todo: handle repeating occurances
-
                         let tree = commit.tree()?;
                         let entry = tree.get_path(target_file_path)?;
                         let blob = repo.find_blob(entry.id())?;
@@ -105,95 +96,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    // // personal import
-    // let repo = git_commits::open(repo_path)?;
-    // for commit in repo.commits()? {
-    //     let commit = commit?;
-    //     println!("\n\n{}\n\n", commit);
-    //     for change in commit.changes()? {
-    //         let change = change?;
-    //         println!(" {} ", change);
-    //     }
-    //     break;
-    // }
-
-    // // git2 scratch approach
-    // let repo = Repository::open(repo_path)?;
-    // let mut revwalk = repo.revwalk()?;
-    // revwalk.push_head()?;
-    // revwalk.set_sorting(Sort::NONE)?;
-    // let oid = revwalk.next();
-    // if let Some(oid) = oid {
-    //     let oid = oid?;
-    //     let commit = repo.find_commit(oid)?;
-    //     println!("{} ", commit.author());
-    // }
-
-    // // git-commits approach
-    // let repo = git_commits::open(repo_path)?;
-    // for commit in repo.commits()? {
-    //     let commit = commit?;
-    //     println!("{}", commit);
-    //     println!("SHA: {}", commit.sha());
-    //     for change in commit.changes()? {
-    //         let change = change?;
-    //         println!("{}", change);
-    //         if let Some(added) = change.as_added() {
-    //             println!("Added: {}", added);
-    //         }
-    //         break;
-    //     }
-    //     break;
-    // }
-
-
-    Ok(())
-}
-
-fn print_test_commit_diff(repo_path: &str) -> Result<(), git2::Error> {
-    let repo = Repository::open(repo_path)?;
-
-    let mut revwalk = repo.revwalk()?;
-    revwalk.push_head()?;
-    revwalk.set_sorting(Sort::TOPOLOGICAL)?;
-
-    for oid in revwalk {
-        let commit = repo.find_commit(oid?)?;
-        println!("Commit: {} by '{}' ({:?})", commit.id(), commit.author(), commit.summary());
-
-        if commit.parent_count() > 0 {
-            let parent = commit.parent(0)?;
-            let mut opts = DiffOptions::new();
-
-            let diff = repo.diff_tree_to_tree(Some(&mut parent.tree()?), Some(&mut commit.tree()?), Some(&mut opts))?;
-            diff.print(git2::DiffFormat::Patch, | delta, hunk, line | {
-                let hunk = match hunk {
-                    Some(hunk) => hunk,
-                    None => return true,
-                };
-                let default_decode = "[invalid utf]";
-
-                let path_bytes = match delta.new_file().path_bytes() {
-                    Some(path_bytes) => path_bytes,
-                    None => return true,
-                };
-                println!(
-                    "
-                    \n
-                    \nDELTA: {}
-                    \n_HUNK: {}
-                    \n_LINE: {}
-                    \n
-                    ",
-                    str::from_utf8(path_bytes).unwrap_or(default_decode),
-                    str::from_utf8(hunk.header()).unwrap_or(default_decode),
-                    str::from_utf8(line.content()).unwrap_or(default_decode)
-                );
-                true
-            })?;
-        }
-
-        break;
-    }
     Ok(())
 }
